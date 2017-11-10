bf49b53a4814b8c14a9d376e248eff2aee92eb75
Carlo Nicolò <carlo.nicolo@studenti.unitn.it>
2017-11-10 23:24:32 +0100

    New Vagrant file with scripts

---
+++ script.sh
@@ -0,0 +1,9 @@
+cd /vagrant/projects/healthservices-waitingtimes
+
+touch deployment.run
+
+echo "mkdir .log 2> /dev/null" >> deployment.run
+
+echo "DEBUG=True authbind gunicorn -b 0.0.0.0:8000 app:app --reload --access-logfile .log/access.log --error-logfile .log/general.log --log-file=-" >> deployment.run
+
+chmod +x deployment.run
